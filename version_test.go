3800421c601261a584bcbd23de5311080165eb9c
Christopher Hall <hsw@bitmark.com>
2015-12-15 10:38:43 +0800

    Add a new -git_describe_match=GLOB option
    The features are:
    * allows for skipping non-version tags
    * assumes version tags are 'v' prefixed and are N, N.N  or N.N....N
      (where N represents a decimal number
    * the argument is a glob to match with a '*' to represent the wildcard
    * example globs: '*' '2.*' '19.*.5'
    * the 'v' is optional in the GLOB i.e. this is ok:  -git_describe_match='v8.*'
      and has the same meaning as: -git_describe_match='8.*'
    
    Signed-off-by: Christopher Hall <hsw@bitmark.com>

--- version_test.go
+++ version_test.go
@@ -42,7 +42,7 @@ func TestSnapshotVersion(t *testing.T) {
 		t.Fatalf("Could not run %v: %v", cmd.Args, err)
 	}
 
-	got, err := pkgVersionFromGit(tempdir)
+	got, err := pkgVersionFromGit(tempdir, "")
 	if err != nil {
 		t.Fatalf("Determining package version from git failed: %v", err)
 	}
@@ -52,7 +52,7 @@ func TestSnapshotVersion(t *testing.T) {
 
 	gitCmdOrFatal(t, tempdir, "tag", "-a", "v1", "-m", "release v1")
 
-	got, err = pkgVersionFromGit(tempdir)
+	got, err = pkgVersionFromGit(tempdir, "")
 	if err != nil {
 		t.Fatalf("Determining package version from git failed: %v", err)
 	}
@@ -72,7 +72,7 @@ func TestSnapshotVersion(t *testing.T) {
 		t.Fatalf("Could not run %v: %v", cmd.Args, err)
 	}
 
-	got, err = pkgVersionFromGit(tempdir)
+	got, err = pkgVersionFromGit(tempdir, "")
 	if err != nil {
 		t.Fatalf("Determining package version from git failed: %v", err)
 	}
@@ -92,11 +92,51 @@ func TestSnapshotVersion(t *testing.T) {
 		t.Fatalf("Could not run %v: %v", cmd.Args, err)
 	}
 
-	got, err = pkgVersionFromGit(tempdir)
+	got, err = pkgVersionFromGit(tempdir, "")
 	if err != nil {
 		t.Fatalf("Determining package version from git failed: %v", err)
 	}
 	if want := "1+git20150508.2."; !strings.HasPrefix(got, want) {
 		t.Logf("got %q, want %q", got, want)
+	}
+
+	// test the describe match glob
+
+	gitCmdOrFatal(t, tempdir, "tag", "-a", "v2.7", "-m", "new version tag")
+
+	if err := ioutil.WriteFile(tempfile, []byte("testcase 4"), 0644); err != nil {
+		t.Fatalf("Could not write temp file %q: %v", tempfile, err)
+	}
+
+	cmd = exec.Command("git", "commit", "-a", "-m", "third change")
+	cmd.Env = append(os.Environ(), "GIT_COMMITTER_DATE=2015-05-09T11:22:33")
+	cmd.Dir = tempdir
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		t.Fatalf("Could not run %v: %v", cmd.Args, err)
+	}
+
+	gitCmdOrFatal(t, tempdir, "tag", "-a", "xyz99.99", "-m", "spurious tag")
+
+	if err := ioutil.WriteFile(tempfile, []byte("testcase 5"), 0644); err != nil {
+		t.Fatalf("Could not write temp file %q: %v", tempfile, err)
+	}
+
+	cmd = exec.Command("git", "commit", "-a", "-m", "fourth change")
+	cmd.Env = append(os.Environ(), "GIT_COMMITTER_DATE=2015-05-09T11:22:33")
+	cmd.Dir = tempdir
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		t.Fatalf("Could not run %v: %v", cmd.Args, err)
+	}
+
+	for _, versionGlob := range []string{"*", "v*", "2.*", "v2.*", "2.7", "v2.7"} {
+		got, err = pkgVersionFromGit(tempdir, versionGlob)
+		if err != nil {
+			t.Fatalf("Determining package version from git failed: %v", err)
+		}
+		if want := "2.7-2-g"; !strings.HasPrefix(got, want) {
+			t.Logf("got %q, want %q", got, want)
+		}
 	}
 }
