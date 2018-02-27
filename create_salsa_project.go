b57c318333d55bf3c99c7feea5a4483fe7548733
Michael Stapelberg <michael@stapelberg.de>
2018-02-27 22:02:21 +0100

    create salsa projects via a request to pgt-api-server
    The source code of the corresponding server can be found at:
    https://github.com/Debian/pkg-go-tools/tree/master/cmd/pgt-api-server
    
    The repository configuration can be found at:
    https://salsa.debian.org/go-team/ci/blob/master/config/config.go

--- create_salsa_project.go
+++ create_salsa_project.go
@@ -1,110 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"flag"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
-	"strconv"
 )
-
-const (
-	salsaGroupID = 2638
-	salsaApiUrl  = "https://salsa.debian.org/api/v4"
-)
-
-type gitlabProject struct {
-	ID            int    `json:"id"`
-	Name          string `json:"name"`
-	HttpUrlToRepo string `json:"http_url_to_repo"`
-}
 
 func execCreateSalsaProject(args []string) {
-	projectName := mustGetProjectName(args)
-	token := mustGetSalsaToken()
-
-	gitlabProject, err := createSalsaProject(projectName, token)
-	if err != nil {
-		log.Fatalf("Could not create %s on salsa: %s\n", projectName, err)
-	}
-
-	fmt.Printf("Project %s was created on salsa.debian.org: %s\n",
-		gitlabProject.Name,
-		gitlabProject.HttpUrlToRepo)
-
-	if err := createSalsaWebhook(gitlabProject.ID,
-		"https://webhook.salsa.debian.org/tagpending/"+gitlabProject.Name,
-		token); err != nil {
-		log.Fatalf("Could not create webhook on salsa project: %s\n", err)
-	}
-
-	for _, branch := range []string{"master", "debian/*", "upstream", "upstream/*", "pristine-tar"} {
-		if err := protectSalsaProjectBranch(gitlabProject.ID, branch, token); err != nil {
-			log.Printf("Could not protect branch %s: %s\n", branch, err)
-		}
-	}
-
-}
-
-func postFormToSalsaApi(path string, data url.Values, token string) (*http.Response, error) {
-	postUrl := salsaApiUrl + path
-	data.Add("private_token", token)
-	return http.PostForm(postUrl, data)
-}
-
-func protectSalsaProjectBranch(projectId int, branch, token string) error {
-	postPath := "/projects/" + strconv.Itoa(projectId) + "/protected_branches"
-
-	response, err := postFormToSalsaApi(postPath,
-		url.Values{
-			"name": {branch},
-		},
-		token)
-	if err != nil {
-		return err
-	}
-
-	if response.StatusCode != http.StatusCreated {
-		responseData, _ := ioutil.ReadAll(response.Body)
-
-		return fmt.Errorf("http status %d: %s",
-			response.StatusCode,
-			responseData)
-	}
-
-	return nil
-}
-
-func createSalsaWebhook(projectId int, webhookUrl, token string) error {
-	postPath := "/projects/" + strconv.Itoa(projectId) + "/hooks"
-
-	response, err := postFormToSalsaApi(postPath,
-		url.Values{
-			"url":         {webhookUrl},
-			"push_events": {"true"},
-		},
-		token)
-	if err != nil {
-		return err
-	}
-
-	if response.StatusCode != http.StatusCreated {
-		responseData, _ := ioutil.ReadAll(response.Body)
-
-		return fmt.Errorf("http status %d: %s",
-			response.StatusCode,
-			responseData)
-	}
-
-	return nil
-}
-
-func mustGetProjectName(args []string) string {
 	fs := flag.NewFlagSet("search", flag.ExitOnError)
 
 	if err := fs.Parse(args); err != nil {
@@ -118,46 +23,19 @@ func mustGetProjectName(args []string) string {
 
 	projectName := fs.Arg(0)
 
-	return projectName
-}
+	// The source code of the corresponding server can be found at:
+	// https://github.com/Debian/pkg-go-tools/tree/master/cmd/pgt-api-server
+	u, _ := url.Parse("https://pgt-api-server.debian.net/v1/createrepo")
+	q := u.Query()
+	q.Set("repo", projectName)
+	u.RawQuery = q.Encode()
 
-func mustGetSalsaToken() string {
-	token := os.Getenv("SALSA_TOKEN")
-	if token == "" {
-		log.Printf("Please set the SALSA_TOKEN environment variable.\n")
-		log.Fatalf("Obtain it from the following page: https://salsa.debian.org/profile/personal_access_tokens\n")
-	}
-	return token
-}
-
-func createSalsaProject(projectName, token string) (*gitlabProject, error) {
-	response, err := postFormToSalsaApi("/projects",
-		url.Values{
-			"private_token": {token},
-			"path":          {projectName},
-			"namespace_id":  {strconv.Itoa(salsaGroupID)},
-			"description":   {fmt.Sprintf("Debian packaging for %s", projectName)},
-			"visibility":    {"public"},
-		},
-		token)
+	resp, err := http.Post(u.String(), "", nil)
 	if err != nil {
-		return nil, err
+		log.Fatal(err)
 	}
-
-	if response.StatusCode != http.StatusCreated {
-		responseData, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			return nil, err
-		}
-
-		return nil, errors.New(fmt.Sprintf("http status %d: %s", response.StatusCode, responseData))
+	if got, want := resp.StatusCode, http.StatusOK; got != want {
+		b, _ := ioutil.ReadAll(resp.Body)
+		log.Fatalf("unexpected HTTP status code: got %d, want %d (response: %s)", got, want, string(b))
 	}
-
-	var project gitlabProject
-
-	if err := json.NewDecoder(response.Body).Decode(&project); err != nil {
-		return nil, err
-	}
-
-	return &project, err
 }
