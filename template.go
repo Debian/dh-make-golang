Debian/dh-make-golang

commit 8ed6b21ddc9db74be01c013826bb7095abd64e10

Add -type=library+program and -type=program+library
a.k.a. -type=both and -type=combined respectively,
for packages containing both library and program,
see #814690.

Fixes #57

template.go
@@ -10,7 +10,7 @@ import (
 	"time"
 )
 
-func writeTemplates(dir, gopkg, debsrc, debbin, debversion, pkgType string, dependencies []string, vendorDirs []string) error {
+func writeTemplates(dir, gopkg, debsrc, debLib, debProg, debversion string, pkgType packageType, dependencies []string, vendorDirs []string) error {
 	if err := os.Mkdir(filepath.Join(dir, "debian"), 0755); err != nil {
 		return err
 	}
@@ -21,7 +21,7 @@ func writeTemplates(dir, gopkg, debsrc, debbin, debversion, pkgType string, depe
 	if err := writeDebianChangelog(dir, debsrc, debversion); err != nil {
 		return err
 	}
-	if err := writeDebianControl(dir, gopkg, debsrc, debbin, pkgType, dependencies); err != nil {
+	if err := writeDebianControl(dir, gopkg, debsrc, debLib, debProg, pkgType, dependencies); err != nil {
 		return err
 	}
 	if err := writeDebianCopyright(dir, gopkg, vendorDirs); err != nil {
@@ -37,6 +37,9 @@ func writeTemplates(dir, gopkg, debsrc, debbin, debversion, pkgType string, depe
 		return err
 	}
 	if err := writeDebianWatch(dir, gopkg, debsrc); err != nil {
+		return err
+	}
+	if err := writeDebianPackageInstall(dir, debLib, debProg, pkgType); err != nil {
 		return err
 	}
 	return nil
@@ -61,12 +64,69 @@ func writeDebianChangelog(dir, debsrc, debversion string) error {
 	return nil
 }
 
-func writeDebianControl(dir, gopkg, debsrc, debbin, pkgType string, dependencies []string) error {
+func fprintfControlField(f *os.File, field string, valueArray []string) {
+	switch wrapAndSort {
+	case "a":
+		// Current default, also what "cme fix dpkg" generates
+		fmt.Fprintf(f, "%s: %s\n", field, strings.Join(valueArray, ",\n"+strings.Repeat(" ", len(field)+2)))
+	case "at", "ta":
+		// -t, --trailing-comma, preferred by Martina Ferrari
+		// and currently used in quite a few packages
+		fmt.Fprintf(f, "%s: %s,\n", field, strings.Join(valueArray, ",\n"+strings.Repeat(" ", len(field)+2)))
+	case "ast", "ats", "sat", "sta", "tas", "tsa":
+		// -s, --short-indent too, proposed by Guillem Jover
+		fmt.Fprintf(f, "%s:\n %s,\n", field, strings.Join(valueArray, ",\n "))
+	default:
+		log.Fatalf("%q is not a valid value for -wrap-and-sort, aborting.", wrapAndSort)
+	}
+}
+
+func addDescription(f *os.File, gopkg, comment string) {
+	description, err := getDescriptionForGopkg(gopkg)
+	if err != nil {
+		log.Printf("Could not determine description for %q: %v\n", gopkg, err)
+		description = "TODO: short description"
+	}
+	fmt.Fprintf(f, "Description: %s %s\n", description, comment)
+	longdescription, err := getLongDescriptionForGopkg(gopkg)
+	if err != nil {
+		log.Printf("Could not determine long description for %q: %v\n", gopkg, err)
+		longdescription = "TODO: long description"
+	}
+	fmt.Fprintf(f, " %s\n", longdescription)
+}
+
+func addLibraryPackage(f *os.File, gopkg, debLib string, dependencies []string) {
+	fmt.Fprintf(f, "\n")
+	fmt.Fprintf(f, "Package: %s\n", debLib)
+	deps := []string{"${misc:Depends}"}
+	fmt.Fprintf(f, "Architecture: all\n")
+	deps = append(deps, dependencies...)
+	sort.Strings(deps)
+	fprintfControlField(f, "Depends", deps)
+	addDescription(f, gopkg, "(library)")
+}
+
+func addProgramPackage(f *os.File, gopkg, debProg string, dependencies []string) {
+	fmt.Fprintf(f, "\n")
+	fmt.Fprintf(f, "Package: %s\n", debProg)
+	deps := []string{"${misc:Depends}"}
+	fmt.Fprintf(f, "Architecture: any\n")
+	deps = append(deps, "${shlibs:Depends}")
+	sort.Strings(deps)
+	fprintfControlField(f, "Depends", deps)
+	fmt.Fprintf(f, "Built-Using: ${misc:Built-Using}\n")
+	addDescription(f, gopkg, "(program)")
+}
+
+func writeDebianControl(dir, gopkg, debsrc, debLib, debProg string, pkgType packageType, dependencies []string) error {
 	f, err := os.Create(filepath.Join(dir, "debian", "control"))
 	if err != nil {
 		return err
 	}
 	defer f.Close()
+
+	// Source package:
 
 	fmt.Fprintf(f, "Source: %s\n", debsrc)
 	fmt.Fprintf(f, "Maintainer: Debian Go Packaging Team <team+pkg-go@tracker.debian.org>\n")
@@ -75,48 +135,48 @@ func writeDebianControl(dir, gopkg, debsrc, debbin, pkgType string, dependencies
 	fmt.Fprintf(f, "Section: devel\n")
 	fmt.Fprintf(f, "Testsuite: autopkgtest-pkg-go\n")
 	fmt.Fprintf(f, "Priority: optional\n")
+
 	builddeps := []string{"debhelper-compat (= 12)", "dh-golang"}
 	builddepsByType := append([]string{"golang-any"}, dependencies...)
 	sort.Strings(builddepsByType)
-	fprintfControlField(f, "Build-Depends", builddeps)
-	builddepsDepType := "Indep"
-	if pkgType == "program" {
-		builddepsDepType = "Arch"
+	switch pkgType {
+	case typeLibrary, typeProgram:
+		fprintfControlField(f, "Build-Depends", builddeps)
+		builddepsDepType := "Indep"
+		if pkgType == typeProgram {
+			builddepsDepType = "Arch"
+		}
+		fprintfControlField(f, "Build-Depends-"+builddepsDepType, builddepsByType)
+	case typeLibraryProgram, typeProgramLibrary:
+		builddeps = append(builddeps, builddepsByType...)
+		fprintfControlField(f, "Build-Depends", builddeps)
+	default:
+		log.Fatalf("Invalid pkgType %d in writeDebianControl(), aborting", pkgType)
 	}
-	fprintfControlField(f, "Build-Depends-"+builddepsDepType, builddepsByType)
+
 	fmt.Fprintf(f, "Standards-Version: 4.4.1\n")
 	fmt.Fprintf(f, "Vcs-Browser: https://salsa.debian.org/go-team/packages/%s\n", debsrc)
 	fmt.Fprintf(f, "Vcs-Git: https://salsa.debian.org/go-team/packages/%s.git\n", debsrc)
 	fmt.Fprintf(f, "Homepage: %s\n", getHomepageForGopkg(gopkg))
 	fmt.Fprintf(f, "Rules-Requires-Root: no\n")
 	fmt.Fprintf(f, "XS-Go-Import-Path: %s\n", gopkg)
-	fmt.Fprintf(f, "\n")
-	fmt.Fprintf(f, "Package: %s\n", debbin)
-	deps := []string{"${misc:Depends}"}
-	if pkgType == "program" {
-		fmt.Fprintf(f, "Architecture: any\n")
-		deps = append(deps, "${shlibs:Depends}")
-	} else {
-		fmt.Fprintf(f, "Architecture: all\n")
-		deps = append(deps, dependencies...)
+
+	// Binary package(s):
+
+	switch pkgType {
+	case typeLibrary:
+		addLibraryPackage(f, gopkg, debLib, dependencies)
+	case typeProgram:
+		addProgramPackage(f, gopkg, debProg, dependencies)
+	case typeLibraryProgram:
+		addLibraryPackage(f, gopkg, debLib, dependencies)
+		addProgramPackage(f, gopkg, debProg, dependencies)
+	case typeProgramLibrary:
+		addProgramPackage(f, gopkg, debProg, dependencies)
+		addLibraryPackage(f, gopkg, debLib, dependencies)
+	default:
+		log.Fatalf("Invalid pkgType %d in writeDebianControl(), aborting", pkgType)
 	}
-	sort.Strings(deps)
-	fprintfControlField(f, "Depends", deps)
-	if pkgType == "program" {
-		fmt.Fprintf(f, "Built-Using: ${misc:Built-Using}\n")
-	}
-	description, err := getDescriptionForGopkg(gopkg)
-	if err != nil {
-		log.Printf("Could not determine description for %q: %v\n", gopkg, err)
-		description = "TODO: short description"
-	}
-	fmt.Fprintf(f, "Description: %s\n", description)
-	longdescription, err := getLongDescriptionForGopkg(gopkg)
-	if err != nil {
-		log.Printf("Could not determine long description for %q: %v\n", gopkg, err)
-		longdescription = "TODO: long description"
-	}
-	fmt.Fprintf(f, " %s\n", longdescription)
 
 	return nil
 }
@@ -165,7 +225,7 @@ func writeDebianCopyright(dir, gopkg string, vendorDirs []string) error {
 	return nil
 }
 
-func writeDebianRules(dir, pkgType string) error {
+func writeDebianRules(dir string, pkgType packageType) error {
 	f, err := os.Create(filepath.Join(dir, "debian", "rules"))
 	if err != nil {
 		return err
@@ -176,7 +236,7 @@ func writeDebianRules(dir, pkgType string) error {
 	fmt.Fprintf(f, "\n")
 	fmt.Fprintf(f, "%%:\n")
 	fmt.Fprintf(f, "\tdh $@ --builddirectory=_build --buildsystem=golang --with=golang\n")
-	if pkgType == "program" {
+	if pkgType == typeProgram {
 		fmt.Fprintf(f, "\n")
 		fmt.Fprintf(f, "override_dh_auto_install:\n")
 		fmt.Fprintf(f, "\tdh_auto_install -- --no-source\n")
@@ -233,19 +293,21 @@ func writeDebianWatch(dir, gopkg, debsrc string) error {
 	return nil
 }
 
-func fprintfControlField(f *os.File, field string, valueArray []string) {
-	switch wrapAndSort {
-	case "a":
-		// Current default, also what "cme fix dpkg" generates
-		fmt.Fprintf(f, "%s: %s\n", field, strings.Join(valueArray, ",\n"+strings.Repeat(" ", len(field)+2)))
-	case "at", "ta":
-		// -t, --trailing-comma, preferred by Martina Ferrari
-		// and currently used in quite a few packages
-		fmt.Fprintf(f, "%s: %s,\n", field, strings.Join(valueArray, ",\n"+strings.Repeat(" ", len(field)+2)))
-	case "ast", "ats", "sat", "sta", "tas", "tsa":
-		// -s, --short-indent too, proposed by Guillem Jover
-		fmt.Fprintf(f, "%s:\n %s,\n", field, strings.Join(valueArray, ",\n "))
-	default:
-		log.Fatalf("%q is not a valid value for -wrap-and-sort, aborting.", wrapAndSort)
+func writeDebianPackageInstall(dir, debLib, debProg string, pkgType packageType) error {
+	if pkgType == typeLibraryProgram || pkgType == typeProgramLibrary {
+		f, err := os.Create(filepath.Join(dir, "debian", debProg+".install"))
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		fmt.Fprintf(f, "usr/bin\n")
+
+		f, err = os.Create(filepath.Join(dir, "debian", debLib+".install"))
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		fmt.Fprintf(f, "usr/share\n")
 	}
+	return nil
 }

make.go
@@ -17,6 +17,16 @@ import (
 	"golang.org/x/tools/go/vcs"
 )
 
+type packageType int
+
+const (
+	typeGuess packageType = iota
+	typeLibrary
+	typeProgram
+	typeLibraryProgram
+	typeProgramLibrary
+)
+
 var (
 	dep14       bool
 	pristineTar bool
@@ -388,39 +398,40 @@ func normalizeDebianProgramName(str string) string {
 }
 
 // This follows https://fedoraproject.org/wiki/PackagingDrafts/Go#Package_Names
-func debianNameFromGopkg(gopkg, t string, allowUnknownHoster bool) string {
+func debianNameFromGopkg(gopkg string, t packageType, allowUnknownHoster bool) string {
 	parts := strings.Split(gopkg, "/")
 
-	if t == "program" {
+	if t == typeProgram || t == typeProgramLibrary {
 		return normalizeDebianProgramName(parts[len(parts)-1])
 	}
 
 	host := parts[0]
-	if host == "github.com" {
+	switch host {
+	case "github.com":
 		host = "github"
-	} else if host == "code.google.com" {
+	case "code.google.com":
 		host = "googlecode"
-	} else if host == "cloud.google.com" {
+	case "cloud.google.com":
 		host = "googlecloud"
-	} else if host == "gopkg.in" {
+	case "gopkg.in":
 		host = "gopkg"
-	} else if host == "golang.org" {
+	case "golang.org":
 		host = "golang"
-	} else if host == "google.golang.org" {
+	case "google.golang.org":
 		host = "google"
-	} else if host == "bitbucket.org" {
+	case "bitbucket.org":
 		host = "bitbucket"
-	} else if host == "bazil.org" {
+	case "bazil.org":
 		host = "bazil"
-	} else if host == "pault.ag" {
+	case "pault.ag":
 		host = "pault"
-	} else if host == "howett.net" {
+	case "howett.net":
 		host = "howett"
-	} else if host == "go4.org" {
+	case "go4.org":
 		host = "go4"
-	} else if host == "salsa.debian.org" {
+	case "salsa.debian.org":
 		host = "debian"
-	} else {
+	default:
 		if allowUnknownHoster {
 			suffix, _ := publicsuffix.PublicSuffix(host)
 			host = host[:len(host)-len(suffix)-len(".")]
@@ -574,11 +585,15 @@ func execMake(args []string, usage func()) {
 		false,
 		"Keep using a pristine-tar branch as in the old workflow.\nStrongly discouraged, see \"pristine-tar considered harmful\"\n https://michael.stapelberg.ch/posts/2018-01-28-pristine-tar/\nand the \"Drop pristine-tar branches\" section at\nhttps://go-team.pages.debian.net/workflow-changes.html")
 
-	var pkgType string
-	fs.StringVar(&pkgType,
+	var pkgTypeString string
+	fs.StringVar(&pkgTypeString,
 		"type",
 		"",
-		"One of \"library\" or \"program\"")
+		"Set package type, one of:\n"+
+			` * "library" (aliases: "lib", "l", "dev")`+"\n"+
+			` * "program" (aliases: "prog", "p")`+"\n"+
+			` * "library+program" (aliases: "lib+prog", "l+p", "both")`+"\n"+
+			` * "program+library" (aliases: "prog+lib", "p+l", "combined")`)
 
 	fs.StringVar(&wrapAndSort,
 		"wrap-and-sort",
@@ -608,9 +623,33 @@ func execMake(args []string, usage func()) {
 		gopkg = rr.Root
 	}
 
-	debsrc := debianNameFromGopkg(gopkg, "library", allowUnknownHoster)
+	// Set default source and binary package names.
+	// Note that debsrc may change depending on the actual package type.
+	debsrc := debianNameFromGopkg(gopkg, typeLibrary, allowUnknownHoster)
+	debLib := debsrc + "-dev"
+	debProg := debianNameFromGopkg(gopkg, typeProgram, allowUnknownHoster)
 
-	if strings.TrimSpace(pkgType) != "" {
+	var pkgType packageType
+
+	switch strings.TrimSpace(pkgTypeString) {
+	case "", "guess":
+		pkgType = typeGuess
+	case "library", "lib", "l", "dev":
+		pkgType = typeLibrary
+	case "program", "prog", "p":
+		pkgType = typeProgram
+	case "library+program", "lib+prog", "l+p", "both":
+		// Example packages: golang-github-alecthomas-chroma,
+		// golang-github-tdewolff-minify, golang-github-spf13-viper
+		pkgType = typeLibraryProgram
+	case "program+library", "prog+lib", "p+l", "combined":
+		// Example package: hugo
+		pkgType = typeProgramLibrary
+	default:
+		log.Fatalf("-type=%q not recognized, aborting\n", pkgTypeString)
+	}
+
+	if pkgType != typeGuess {
 		debsrc = debianNameFromGopkg(gopkg, pkgType, allowUnknownHoster)
 		if _, err := os.Stat(debsrc); err == nil {
 			log.Fatalf("Output directory %q already exists, aborting\n", debsrc)
@@ -644,18 +683,14 @@ func execMake(args []string, usage func()) {
 		log.Fatalf("Could not create a tarball of the upstream source: %v\n", err)
 	}
 
-	if strings.TrimSpace(pkgType) == "" {
+	if pkgType == typeGuess {
 		if u.firstMain != "" {
 			log.Printf("Assuming you are packaging a program (because %q defines a main package), use -type to override\n", u.firstMain)
-			pkgType = "program"
+			pkgType = typeProgram
 			debsrc = debianNameFromGopkg(gopkg, pkgType, allowUnknownHoster)
 		} else {
-			pkgType = "library"
+			pkgType = typeLibrary
 		}
-	}
-	debbin := debsrc + "-dev"
-	if pkgType == "program" {
-		debbin = debsrc
 	}
 
 	if _, err := os.Stat(debsrc); err == nil {
@@ -692,7 +727,7 @@ func execMake(args []string, usage func()) {
 	for _, dep := range u.repoDeps {
 		if len(golangBinaries) == 0 {
 			// fall back to heuristic
-			debdependencies = append(debdependencies, debianNameFromGopkg(dep, "library", allowUnknownHoster)+"-dev")
+			debdependencies = append(debdependencies, debianNameFromGopkg(dep, typeLibrary, allowUnknownHoster)+"-dev")
 			continue
 		}
 		bin, ok := golangBinaries[dep]
@@ -703,7 +738,7 @@ func execMake(args []string, usage func()) {
 		debdependencies = append(debdependencies, bin)
 	}
 
-	if err := writeTemplates(dir, gopkg, debsrc, debbin, debversion, pkgType, debdependencies, u.vendorDirs); err != nil {
+	if err := writeTemplates(dir, gopkg, debsrc, debLib, debProg, debversion, pkgType, debdependencies, u.vendorDirs); err != nil {
 		log.Fatalf("Could not create debian/ from templates: %v\n", err)
 	}
 
@@ -714,6 +749,20 @@ func execMake(args []string, usage func()) {
 
 	log.Printf("\n")
 	log.Printf("Packaging successfully created in %s\n", dir)
+	log.Printf("\n")
+	log.Printf("    Source: %s\n", debsrc)
+	switch pkgType {
+	case typeLibrary:
+		log.Printf("    Package: %s\n", debLib)
+	case typeProgram:
+		log.Printf("    Package: %s\n", debProg)
+	case typeLibraryProgram:
+		log.Printf("    Package: %s\n", debLib)
+		log.Printf("    Package: %s\n", debProg)
+	case typeProgramLibrary:
+		log.Printf("    Package: %s\n", debProg)
+		log.Printf("    Package: %s\n", debLib)
+	}
 	log.Printf("\n")
 	log.Printf("Resolve all TODOs in %s, then email it out:\n", itpname)
 	log.Printf("    sendmail -t < %s\n", itpname)

make_test.go
@@ -46,12 +46,12 @@ func TestNormalizeDebianProgramName(t *testing.T) {
 
 var nameFromGoPkg = []struct {
 	in  string
-	t   string
+	t   packageType
 	out string
 }{
-	{"github.com/dh-make-golang", "program", "dh-make-golang"},
-	{"github.com/DH-make-golang", "", "golang-github-dh-make-golang"},
-	{"github.com/dh_make_golang", "", "golang-github-dh-make-golang"},
+	{"github.com/Debian/dh-make-golang", typeProgram, "dh-make-golang"},
+	{"github.com/Debian/DH-make-golang", typeGuess, "golang-github-debian-dh-make-golang"},
+	{"github.com/Debian/dh_make_golang", typeGuess, "golang-github-debian-dh-make-golang"},
 }
 
 func TestDebianNameFromGopkg(t *testing.T) {

commit 82916c0d56b6319398f635199222dff397fafc12

Remove unneeded golang-*-dev dependency for program
This fixes a bug that I introduced in commit 8ed6b21 for v0.1.0

template.go
@@ -112,13 +112,11 @@ func addLibraryPackage(f *os.File, gopkg, debLib string, dependencies []string) 
 	addDescription(f, gopkg, "(library)")
 }
 
-func addProgramPackage(f *os.File, gopkg, debProg string, dependencies []string) {
+func addProgramPackage(f *os.File, gopkg, debProg string) {
 	fmt.Fprintf(f, "\n")
 	fmt.Fprintf(f, "Package: %s\n", debProg)
 	fmt.Fprintf(f, "Architecture: any\n")
-	deps := dependencies
-	sort.Strings(deps)
-	deps = append(deps, "${misc:Depends}", "${shlibs:Depends}")
+	deps := []string{"${misc:Depends}", "${shlibs:Depends}"}
 	fprintfControlField(f, "Depends", deps)
 	fmt.Fprintf(f, "Built-Using: ${misc:Built-Using}\n")
 	addDescription(f, gopkg, "(program)")
@@ -172,12 +170,12 @@ func writeDebianControl(dir, gopkg, debsrc, debLib, debProg string, pkgType pack
 	case typeLibrary:
 		addLibraryPackage(f, gopkg, debLib, dependencies)
 	case typeProgram:
-		addProgramPackage(f, gopkg, debProg, dependencies)
+		addProgramPackage(f, gopkg, debProg)
 	case typeLibraryProgram:
 		addLibraryPackage(f, gopkg, debLib, dependencies)
-		addProgramPackage(f, gopkg, debProg, dependencies)
+		addProgramPackage(f, gopkg, debProg)
 	case typeProgramLibrary:
-		addProgramPackage(f, gopkg, debProg, dependencies)
+		addProgramPackage(f, gopkg, debProg)
 		addLibraryPackage(f, gopkg, debLib, dependencies)
 	default:
 		log.Fatalf("Invalid pkgType %d in writeDebianControl(), aborting", pkgType)