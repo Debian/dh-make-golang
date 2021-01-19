a9e6eaa8d4aef52d6aa8befb397075a3da2f04ab
Arnaud Rebillout <elboulangero@gmail.com>
2021-01-19 11:08:58 +0700

    Set debianBranch in execMake(), use it in createGitRepository()
    This minor refactoring prepares the code for the next commit, where we
    will need to know the Debian branch in `execMake()`.

--- make.go
+++ make.go
@@ -382,7 +382,7 @@ func runGitCommandIn(dir string, arg ...string) error {
 }
 
 func createGitRepository(debsrc, gopkg, orig string, u *upstream,
-	includeUpstreamHistory, allowUnknownHoster, dep14, pristineTar bool) (string, error) {
+	includeUpstreamHistory bool, allowUnknownHoster bool, debianBranch string, pristineTar bool) (string, error) {
 	wd, err := os.Getwd()
 	if err != nil {
 		return "", err
@@ -392,14 +392,8 @@ func createGitRepository(debsrc, gopkg, orig string, u *upstream,
 		return "", err
 	}
 
-	if err := runGitCommandIn(dir, "init"); err != nil {
+	if err := runGitCommandIn(dir, "init", "-b", debianBranch); err != nil {
 		return dir, err
-	}
-
-	if dep14 {
-		if err := runGitCommandIn(dir, "checkout", "-q", "-b", "debian/sid"); err != nil {
-			return dir, err
-		}
 	}
 
 	// Set repository options
@@ -434,12 +428,6 @@ func createGitRepository(debsrc, gopkg, orig string, u *upstream,
 
 	// Preconfigure branches
 
-	var debianBranch string
-	if dep14 {
-		debianBranch = "debian/sid"
-	} else {
-		debianBranch = "master"
-	}
 	branches := []string{debianBranch, "upstream"}
 	if pristineTar {
 		branches = append(branches, "pristine-tar")
@@ -470,10 +458,7 @@ func createGitRepository(debsrc, gopkg, orig string, u *upstream,
 
 	// Import upstream orig tarball
 
-	arg := []string{"import-orig", "--no-interactive"}
-	if dep14 {
-		arg = append(arg, "--debian-branch=debian/sid")
-	}
+	arg := []string{"import-orig", "--no-interactive", "--debian-branch=" + debianBranch}
 	if pristineTar {
 		arg = append(arg, "--pristine-tar")
 	}
@@ -843,6 +828,12 @@ func execMake(args []string, usage func()) {
 		log.Fatalf("-type=%q not recognized, aborting\n", pkgTypeString)
 	}
 
+	// Set the debian branch.
+	debBranch := "master"
+	if dep14 {
+		debBranch = "debian/sid"
+	}
+
 	switch strings.TrimSpace(wrapAndSort) {
 	case "a":
 		// Current default, also what "cme fix dpkg" generates
@@ -929,7 +920,7 @@ func execMake(args []string, usage func()) {
 
 	debversion := u.version + "-1"
 
-	dir, err := createGitRepository(debsrc, gopkg, orig, u, includeUpstreamHistory, allowUnknownHoster, dep14, pristineTar)
+	dir, err := createGitRepository(debsrc, gopkg, orig, u, includeUpstreamHistory, allowUnknownHoster, debBranch, pristineTar)
 	if err != nil {
 		log.Fatalf("Could not create git repository: %v\n", err)
 	}
