b4874a884548454f6e297ef20ca811d726fb00d8
Richard Hansen <rhansen@rhansen.org>
2025-10-09 18:19:44 -0400

    Defensively ignore empty Go module paths in XS-Go-Import-Path
    Explicitly allow maintainers to add a trailing comma for VC-friendly
    multiline values:
    
        XS-Go-Import-Path:
         example.com/foo,
         example.com/bar,
    
    Currently all code that consumes the map returned from
    `getGolangBinaries` ignores the empty string entry, so this commit
    should not change behavior at all.  However, this commit improves
    readability by making it easy to see what happens in this corner case;
    developers don't need to look at every caller of `getGolangBinaries`
    to see how the empty string entry is handled.  It is also defensive;
    it avoids surprises for any future consumers of `getGolangBinaries`.

--- search_test.go
+++ search_test.go
@@ -22,6 +22,30 @@ func TestGetGolangBinaries(t *testing.T) {
 			results: nil,
 			want:    map[string]debianPackage{},
 		},
+		// It is unclear whether api.ftp-master.d.o would even return a result for a package whose
+		// metadata field is set to the empty string (as of 2026-02-26 there are no such results), but
+		// test the handling just in case.
+		{
+			desc: "empty",
+			results: []ftpMasterApiResult{{
+				Binary:        "golang-example-foo-dev",
+				MetadataValue: "",
+				Source:        "golang-example-foo",
+			}},
+			want: map[string]debianPackage{},
+		},
+		// It is unclear whether api.ftp-master.d.o would even return a result for a package whose
+		// metadata field is set to a whitespace-only string (as of 2026-02-26 there are no such
+		// results), but test the handling just in case.
+		{
+			desc: "whitespace-only",
+			results: []ftpMasterApiResult{{
+				Binary:        "golang-example-foo-dev",
+				MetadataValue: " \t\n\r",
+				Source:        "golang-example-foo",
+			}},
+			want: map[string]debianPackage{},
+		},
 		{
 			desc: "leading whitespace",
 			results: []ftpMasterApiResult{{
@@ -69,10 +93,65 @@ func TestGetGolangBinaries(t *testing.T) {
 			},
 		},
 		{
+			desc: "only commas",
+			results: []ftpMasterApiResult{{
+				Binary:        "golang-example-foo-dev",
+				MetadataValue: " ,\t,\n",
+				Source:        "golang-example-foo",
+			}},
+			want: map[string]debianPackage{},
+		},
+		{
 			desc: "space around comma",
 			results: []ftpMasterApiResult{{
 				Binary:        "golang-example-foo-dev",
 				MetadataValue: "example.com/foo ,\n\texample.com/bar",
+				Source:        "golang-example-foo",
+			}},
+			want: map[string]debianPackage{
+				"example.com/foo": {
+					binary: "golang-example-foo-dev",
+					source: "golang-example-foo",
+				},
+				"example.com/bar": {
+					binary: "golang-example-foo-dev",
+					source: "golang-example-foo",
+				},
+			},
+		},
+		{
+			desc: "leading comma",
+			results: []ftpMasterApiResult{{
+				Binary:        "golang-example-foo-dev",
+				MetadataValue: ",example.com/foo",
+				Source:        "golang-example-foo",
+			}},
+			want: map[string]debianPackage{
+				"example.com/foo": {
+					binary: "golang-example-foo-dev",
+					source: "golang-example-foo",
+				},
+			},
+		},
+		{
+			desc: "trailing comma",
+			results: []ftpMasterApiResult{{
+				Binary:        "golang-example-foo-dev",
+				MetadataValue: "example.com/foo,",
+				Source:        "golang-example-foo",
+			}},
+			want: map[string]debianPackage{
+				"example.com/foo": {
+					binary: "golang-example-foo-dev",
+					source: "golang-example-foo",
+				},
+			},
+		},
+		{
+			desc: "internal extra comma",
+			results: []ftpMasterApiResult{{
+				Binary:        "golang-example-foo-dev",
+				MetadataValue: "example.com/foo,,example.com/bar",
 				Source:        "golang-example-foo",
 			}},
 			want: map[string]debianPackage{
