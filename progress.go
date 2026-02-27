9548beaa3c9e75feff66faf740a645e90dcf34fe
Richard Hansen <rhansen@rhansen.org>
2026-02-27 17:48:09 -0500

    progress: Propagate errors in `monitorDiskUsage`

--- progress.go
+++ progress.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"strings"
 	"time"
 
 	"github.com/mattn/go-isatty"
+	"golang.org/x/sync/errgroup"
 )
 
 const (
@@ -30,41 +33,60 @@ func humanizeBytes(b int64) string {
 	}
 }
 
-func diskUsage(path string) int64 {
+func diskUsage(path string) (int64, error) {
 	var usage int64
 	walkFn := func(path string, info os.FileInfo, err error) error {
-		if err == nil && info.Mode().IsRegular() {
+		if err != nil {
+			return err
+		}
+		if info.Mode().IsRegular() {
 			usage += info.Size()
 		}
 		return nil
 	}
-	filepath.Walk(path, walkFn)
-	return usage
+	if err := filepath.Walk(path, walkFn); err != nil && !errors.Is(err, fs.ErrNotExist) {
+		return 0, err
+	}
+	return usage, nil
 }
 
 // monitorDiskUsage starts a background goroutine that periodically prints the disk usage of the
-// given directory or file.  Returns a done callback that stops the periodic printing.
-func monitorDiskUsage(prefix, path string) func() {
+// given directory or file.  Returns a done callback that stops the periodic printing.  If there is
+// an error during monitoring, that error is returned from the returned done callback and, when the
+// done callback is called, written to the provided error pointer if the pointer is non-nil and the
+// pointed-to value is nil.
+func monitorDiskUsage(prefix, path string, errp *error) func() error {
 	if !isatty.IsTerminal(os.Stdout.Fd()) {
-		return func() {}
+		return func() error { return nil }
 	}
-	done := make(chan struct{})
-	go func() {
+	gr := &errgroup.Group{}
+	quit := make(chan struct{})
+	gr.Go(func() error {
 		// previous holds how many bytes the previous line contained
 		// so that we can clear it in its entirety.
 		var previous int
 		for {
-			usage := diskUsage(path)
+			usage, err := diskUsage(path)
+			if err != nil {
+				return err
+			}
 			fmt.Printf("\r%s", strings.Repeat(" ", previous))
 			previous, _ = fmt.Printf("\r%s: %s", prefix, humanizeBytes(usage))
 			select {
-			case <-done:
+			case <-quit:
 				fmt.Printf("\r")
-				return
+				return nil
 			case <-time.After(250 * time.Millisecond):
 				break
 			}
 		}
-	}()
-	return func() { close(done) }
+	})
+	return func() error {
+		close(quit)
+		err := gr.Wait()
+		if errp != nil && *errp == nil {
+			*errp = err
+		}
+		return err
+	}
 }
