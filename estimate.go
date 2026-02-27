bfee8dc13a9b58fe221642a807a50479603c5737
Richard Hansen <rhansen@rhansen.org>
2026-02-27 15:30:10 -0500

    estimate: Monitor `$GOPATH/pkg/mod/cache` disk usage, not `$GOPATH`
    This is preparation for a future commit that will change `estimate` to
    use the default `GOMODCACHE`.  Tracking all of `GOPATH` is likely to
    be extremely slow due to the potentially huge number of files unpacked
    to the various `$GOPATH/pkg/mod/$module@$version` subdirectories.
    Tracking `$GOMODCACHE/cache` should be much faster, and it tracks
    how much is downloaded, not how much is download+unpacked.

--- estimate.go
+++ estimate.go
@@ -66,7 +66,7 @@ func getSourcesInNew() (map[string]string, error) {
 func get(gopath, repodir, repo, rev string) error {
 	done := make(chan struct{})
 	defer close(done)
-	go progressSize("go get", gopath, done)
+	go progressSize("go get", filepath.Join(gopath, "pkg", "mod", "cache"), done)
 
 	// As per https://groups.google.com/forum/#!topic/golang-nuts/N5apfenE4m4,
 	// the arguments to “go get” are packages, not repositories. Hence, we
