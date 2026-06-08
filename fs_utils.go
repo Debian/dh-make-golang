79f60b15e975ad7bee433fbdcd72e7ffccf602f7
Devansh-567 <devansh.jay.singh@gmail.com>
2026-06-08 21:24:17 +0530

    fix: prevent nil pointer dereference in forceRemoveAll filepath.Walk callback

--- fs_utils.go
+++ fs_utils.go
@@ -10,17 +10,28 @@ import (
 // harder to remove all the files and directories.
 func forceRemoveAll(path string) error {
 	// first pass to make sure all the directories are writable
-	err := filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
-		if info.IsDir() {
-			return os.Chmod(path, 0777)
-		} else {
-			// remove files by the way
-			return os.Remove(path)
+	err := filepath.Walk(path, func(p string, info fs.FileInfo, err error) error {
+		// IMPORTANT: always check err first
+		if err != nil {
+			return err
 		}
+
+		if info == nil {
+			return nil
+		}
+
+		if info.IsDir() {
+			return os.Chmod(p, 0777)
+		}
+
+		// remove files
+		return os.Remove(p)
 	})
+
 	if err != nil {
 		return err
 	}
+
 	// remove the remaining directories
 	return os.RemoveAll(path)
 }
